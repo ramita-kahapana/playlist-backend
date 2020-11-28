6f8f6535ac54f3a4a3161d22e8ab185d1867eb98
DogeCnx <Saratoga1412@gmail.com>
2020-11-28 10:34:43 +0700

    ADD model and connect playlist

--- app/Models/Playlist.js
+++ app/Models/Playlist.js
@@ -4,6 +4,20 @@
 const Model = use('Model')
 
 class Playlist extends Model {
+  static get primaryKey(){
+    return 'playlists_id'
+}
+static get createdAtColumn() {
+    return 'created_at';
+
+}
+static get updatedAtColumn(){
+    return 'updated_at';
+}
+user(){
+    return this.belongsTo('App/Models/User')
+}
+
 }
 
 module.exports = Playlist
--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,9 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+  playlist() {
+    return this.hasMany('App/Models/Client')
+  }
 }
 
 module.exports = User
