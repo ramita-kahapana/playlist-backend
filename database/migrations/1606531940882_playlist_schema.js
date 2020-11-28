23181c40fcb96bb04181ef02509903652427bbb6
DogeCnx <Saratoga1412@gmail.com>
2020-11-28 10:35:13 +0700

    ADD table playlist

--- database/migrations/1606531940882_playlist_schema.js
+++ database/migrations/1606531940882_playlist_schema.js
@@ -6,8 +6,16 @@ const Schema = use('Schema')
 class PlaylistSchema extends Schema {
   up () {
     this.create('playlists', (table) => {
-      table.increments()
+      table.increments('playlists_id')
+      table.string('type_room',255).notNullable()
+      table.string('user_id',200).unsigned()
       table.timestamps()
+
+      table
+      .foreign('user_id')
+      .references('users.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
     })
   }
 
